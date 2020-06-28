13b4b81dd17b761c40ba796d6fca26a0c422816a
Nikolya-Iv <kolyan41k01@yandex.ru>
2020-06-28 20:04:36 +0300

    final project

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+
+    
+       $('.phone').inputmask('+7 (999) 999-99-99'); 
+    
+    
+    
+
+        //E-mail Ajax Send
+        $("form").submit(function() { //Change
+            var th = $(this);
+            $.ajax({
+                type: "POST",
+                url: "mail.php", //Change
+                data: th.serialize()
+            }).done(function() {
+                alert("Thank you!");
+                setTimeout(function() {
+                    // Done Functions
+                    th.trigger("reset");
+                }, 1000);
+            });
+            return false;
+        });
+    
+    
+
+});
